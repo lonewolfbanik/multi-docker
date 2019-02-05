3ca8b669186ef35f9b4287c144d334af5aaa191d
itsmeBanik <prasanjitbanik38@gmail.com>
2019-02-05 16:41:56 +0530

    Added catch block

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
+  const values = await pgClient.query('SELECT * from values').catch((err) => { console.log(err); });
 
   res.send(values.rows);
 });
